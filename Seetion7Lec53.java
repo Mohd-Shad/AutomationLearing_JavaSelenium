01c20c8f7e1421cc49a519273d536db11b135c8c
Mohd-Shad <shadusmani59@gmail.com>
2025-08-30 22:59:05 +0530

    Add files via upload
    Selenium WebDriver -> Technique to automate Web Elements
    Section 7 -> Learning Handling Static Drop Down
    Section7Lec53 -> Learning looping dropdown UI
    Sect7Lect55 -> Learning Dynamic dropdowns with webdriver API

---
+++ Seetion7Lec53.java
@@ -0,0 +1,32 @@
+package Automationsel5;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
+public class Seetion7Lec53 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		driver.findElement(By.id("divpaxinfo")).click();
+		Thread.sleep(2000);
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
+		int i = 1;
+		while(i<5) {
+			driver.findElement(By.id("hrefIncAdt")).click();
+			i++;
+		}
+		
+		driver.findElement(By.id("btnclosepaxoption")).click();
+		Assert.assertEquals(driver.findElement(By.id("divpaxinfo")).getText(), "5 Adult");
+		System.out.println(driver.findElement(By.id("divpaxinfo")).getText());
+		
+		
+		
+		
+	}
+
+}
